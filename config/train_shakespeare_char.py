def25f40f0c0212a94550033640e7ddf98fb8482
adam.skrodzki <adam.skrodzki@gmail.com>
2026-05-01 10:03:29 +0200

    fix config

--- config/train_shakespeare_char.py
+++ config/train_shakespeare_char.py
@@ -3,7 +3,7 @@
 
 out_dir = 'out-shakespeare-char'
 eval_interval = 250 # keep frequent because we'll overfit
-eval_iters = 200
+eval_iters = 20
 log_interval = 10 # don't print too too often
 compile = True
 
